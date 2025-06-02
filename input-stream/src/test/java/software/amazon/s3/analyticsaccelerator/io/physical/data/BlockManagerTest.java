b7d693f10438c32239bace5bc99b6504f99b0dc2
rajdchak <rajdchak@amazon.co.uk>
2025-06-02 12:55:16 +0100

    Fix bug in small obj prefetching

--- input-stream/src/test/java/software/amazon/s3/analyticsaccelerator/io/physical/data/BlockManagerTest.java
+++ input-stream/src/test/java/software/amazon/s3/analyticsaccelerator/io/physical/data/BlockManagerTest.java
@@ -139,14 +139,25 @@ public class BlockManagerTest {
   }
 
   @Test
-  void testGetBlockIsEmpty() throws IOException {
+  void testGetBlockIsEmptyWhenNotSmallObject() throws IOException {
+    // Given
+    BlockManager blockManager = getTestBlockManager(9 * ONE_MB);
+
+    // When: nothing
+
+    // Then
+    assertFalse(blockManager.getBlock(0).isPresent());
+  }
+
+  @Test
+  void testGetBlockIsNotEmptyWhenSmallObject() throws IOException {
     // Given
     BlockManager blockManager = getTestBlockManager(42);
 
     // When: nothing
 
     // Then
-    assertFalse(blockManager.getBlock(0).isPresent());
+    assertTrue(blockManager.getBlock(0).isPresent());
   }
 
   @Test
@@ -210,7 +221,7 @@ public class BlockManagerTest {
   void testMakeRangeAvailableDoesNotOverread() throws IOException {
     // Given: BM with 0-64KB and 64KB+1 to 128KB
     ObjectClient objectClient = mock(ObjectClient.class);
-    BlockManager blockManager = getTestBlockManager(objectClient, 128 * ONE_KB);
+    BlockManager blockManager = getTestBlockManager(objectClient, 128 * ONE_KB, PhysicalIOConfiguration.builder().smallObjectsPrefetchingEnabled(false).build());
     blockManager.makePositionAvailable(0, ReadMode.SYNC);
     blockManager.makePositionAvailable(64 * ONE_KB + 1, ReadMode.SYNC);
 
@@ -347,10 +358,6 @@ public class BlockManagerTest {
   void testIsBlockStoreEmpty() throws IOException {
     // Given
     BlockManager blockManager = getTestBlockManager(42);
-
-    // When & Then
-    assertTrue(blockManager.isBlockStoreEmpty(), "BlockStore should be empty initially");
-
     // After adding a block
     blockManager.makePositionAvailable(0, ReadMode.SYNC);
     assertFalse(
